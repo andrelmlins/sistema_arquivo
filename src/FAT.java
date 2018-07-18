ac19374db03620b36b5e7704fb4954574ba0462a
Siga Mais <Siga Mais@10.246.199.115>
2018-07-18 20:17:50 -0300

    Hd e Fat

---
+++ src/FAT.java
@@ -0,0 +1,6 @@
+
+public class FAT {
+	
+	String nameFile;
+	int indexBlock;
+}
---
+++ src/HD.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class HD {
+	
+	int tamanho;
+	ArrayList<Integer> blocos;
+	
+	public HD(int tamanho) {
+		super();
+		this.tamanho = tamanho;
+		this.blocos = new ArrayList<>();
+	}
+}
